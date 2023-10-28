71a977def0c52c51b6030e159f4945fe262ac3b5
neenu-as <neenuajithkumar@gmail.com>
2023-10-28 15:25:34 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+void main()
+{
+ runApp(MaterialApp(home: Center(child: Text(" welcome to  flutter" ,style: TextStyle(color: Colors.blue, fontSize: 45,decoration: TextDecoration.none))),));
+
+
+
+
+
+
+
+
+
+
+
+}
